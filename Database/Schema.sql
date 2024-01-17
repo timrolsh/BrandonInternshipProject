97b7c00afa8eb2f818213c49d3b2235dbac7271b
Tim Rolshud <tim.rolsh@gmail.com>
2024-01-16 23:56:25 -0500

    remove foreign key checks from db for simplicity

--- Database/Schema.sql
+++ Database/Schema.sql
@@ -1,15 +1,10 @@
 -- Use our assigned schema
 USE local;
 
--- disable foreign key checks so that tables could be droppped
-SET FOREIGN_KEY_CHECKS = 0;
-
 -- Drop each table individually if they exist
 DROP TABLE IF EXISTS user cascade;
 DROP TABLE IF EXISTS text_input cascade;
 
--- re-enable foreign key checks for normal functionality
-SET FOREIGN_KEY_CHECKS = 1;
 
 create table user
 (
